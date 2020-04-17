8d1f80842f3d65b1e049a76448864cc974373584
camilondero <48757786+camilondero@users.noreply.github.com>
2020-04-17 19:34:15 +0000

    Deploying to gh-pages from  - 2075bd76cefb426e2bda9e66095c0df98daa24b5 🚀

--- raylibTemplate.js.orig.js
+++ raylibTemplate.js.orig.js
@@ -194,7 +194,7 @@ Module['FS_createPath']('/', 'resources', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 1, "end": 2338920, "filename": "/resources/Cyberpunk Moonlight Sonata.mp3"}, {"start": 2338920, "audio": 0, "end": 2349613, "filename": "/resources/shell.html"}, {"start": 2349613, "audio": 0, "end": 2351570, "filename": "/resources/ship.png"}], "remote_package_size": 2351570, "package_uuid": "766b7561-9e6d-4e58-8194-ad5e06170db6"});
+ loadPackage({"files": [{"start": 0, "audio": 1, "end": 2338920, "filename": "/resources/Cyberpunk Moonlight Sonata.mp3"}, {"start": 2338920, "audio": 0, "end": 2349613, "filename": "/resources/shell.html"}, {"start": 2349613, "audio": 0, "end": 2351570, "filename": "/resources/ship.png"}], "remote_package_size": 2351570, "package_uuid": "07f5636a-ed12-4278-9ffc-31f59af1757a"});
 
 })();
 
