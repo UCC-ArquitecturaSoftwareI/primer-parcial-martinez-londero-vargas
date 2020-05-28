ec6d8820eaa4c80567fb9b07218492b20e77b641
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2020-05-28 13:26:45 -0300

    Agregado movimiento a Enemy

--- clases/Enemy.cpp
+++ clases/Enemy.cpp
@@ -15,6 +15,26 @@ void Enemy::draw() {
     anim[0]->draw(Enemy_pos, mirar);
 }
 
+
 const Vector2 &Enemy::getEnemyPos() const {
     return Enemy_pos;
 }
+
+/*
+void Enemy::removeEnemy(Vector2 &EnemyPos) {
+    if (Enemy_pos.x < 2) {
+        draw()
+
+    }
+
+
+}*/
+
+void Enemy::moveEnemy(){
+    Enemy_pos.x=Enemy_pos.x - 1;
+
+}
+/*
+Enemy *Enemy::clone() {
+    return new Enemy();
+}*/
